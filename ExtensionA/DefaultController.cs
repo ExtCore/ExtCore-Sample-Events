4b206875fc7a0460ec481127f5400f69e504e3e8
Dmitry Sikorsky <dmitry@sikorsky.pro>
2017-04-04 14:34:07 +0300

    Added project content

---
+++ ExtensionA/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2017 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using ExtCore.Events;
+using ExtensionA.Events;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExtensionA
+{
+  public class DefaultController : Controller
+  {
+    public ActionResult Index()
+    {
+      Event<ISomeActionEventHandler, string>.Broadcast("Some action has happened at " + DateTime.Now.ToString() + "!");
+      return this.Content("Some content from the extension A.");
+    }
+  }
+}
---
+++ ExtensionB/SomeActionEventHandler.cs
@@ -0,0 +1,16 @@
+﻿// Copyright © 2017 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO;
+using ExtensionA.Events;
+
+namespace ExtensionB
+{
+  public class SomeActionEventHandler : ISomeActionEventHandler
+  {
+    public void HandleEvent(string argument)
+    {
+      File.WriteAllText(@"C:\<Path to the project>\ExtCore-Sample-Events\WebApplication\log.txt", argument);
+    }
+  }
+}
